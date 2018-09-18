032f3b5e5e6e7911caeaf6ef4eaee89fe86fc57b
Mostafa Anwar <39226625+MostafAanwar@users.noreply.github.com>
2018-09-19 01:08:45 +0200

    Add files via upload

---
+++ SecrectMessage.js
@@ -0,0 +1,11 @@
+/*Using array methods, we transform an array of strings into a secret message!*/
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push('to');
+secretMessage.push('program');
+secretMessage[secretMessage.indexOf('easily')]='hard';
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(6, 11, 'know');
+console.log(secretMessage.join(' '));
